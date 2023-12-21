6572e5a68a4f746b228f3fab72d98ee1346796cb
samdarko-xo <samdarkomgmt@gmail.com>
2023-12-21 13:18:20 -0800

    game added

---
+++ app.nw/js/plugins/RMMVNodejs1-1.js
@@ -0,0 +1,14 @@
+//=============================================================================
+//  RMMVNodeP1.js
+//=============================================================================
+
+(function () {
+
+  //Set up function in case we want to split up our code further  
+  function setup() {
+
+  }
+  
+  //Execute set up function to run our code after setup
+  setup();
+})();
---
+++ app.nw/js/plugins/RMMVNodejs1-2.js
@@ -0,0 +1,34 @@
+//=============================================================================
+//  RMMVNodeP1.js
+//=============================================================================
+
+(function () {
+
+  //Setup function in case we want to split up our code further  
+  function setup() {
+//=============================================================================
+//  MVNodeFS
+//=============================================================================    
+    function MVNodeFS() {
+
+    }
+
+    //Wrapper for Node.js writeFileSync
+    MVNodeFS.writeFile = function (filePath, filename, data) {
+      
+    };
+
+    //Wrapper for Node.js readFileSync
+    MVNodeFS.readFile = function (filePath, filename) {
+      
+    };
+
+    //Method for creating the proper file path from the relative file path
+    MVNodeFS.createPath = function (relativePath) {
+      
+    };
+  }
+
+  //Execute setup function to run our code after setup
+  setup();
+})();
